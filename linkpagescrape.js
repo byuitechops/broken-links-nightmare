2d1620c34eaf94d5624cf103660d5d9c39db6cc8
maeashley <ashleystewart8888@yahoo.com>
2017-05-19 12:29:05 -0600

    added stuff

--- linkpagescrape.js
+++ linkpagescrape.js
@@ -4,39 +4,45 @@ var nightmare = new Nightmare({
 });
 var creds = require('./credentials.js')
 //1. Go to the page with all the links
-nightmare
-    .goto('https://byui.brightspace.com/d2l/login?noredirect=true')
-    .type('#userName', creds.username)
-    .type('#password', creds.password)
-    .click('.d2l-button')
-    .wait(3000)
-    .goto('https://byui.brightspace.com/d2l/brokenLinks/6606')
-    //2. Wait 
-    .wait(1000)
-    //3. If the selector button exists, click it
-    .evaluate(function () {
-        function clickLoad() {
-            if ($('.d2l-loadmore-pager:visible').length) {
-                $('.d2l-loadmore-pager').click()
-                setTimeout(clickLoad, 500)
+function startNightmare(nightmare) {
+    nightmare
+        .goto('https://byui.brightspace.com/d2l/login?noredirect=true')
+        .type('#userName', creds.username)
+        .type('#password', creds.password)
+        .click('.d2l-button')
+        .wait(3000)
+        .goto('https://byui.brightspace.com/d2l/brokenLinks/6606')
+        //2. Wait for page to load
+        .wait(1000)
+        //3. If the selector button exists, click it until it no longer exists
+        .evaluate(function () {
+            function clickLoad() {
+                if ($('.d2l-loadmore-pager:visible').length) {
+                    $('.d2l-loadmore-pager').click()
+                    setTimeout(clickLoad, 500)
+                }
             }
-        }
-        clickLoad()
-    })
-    //4. When it no longer exists, scrape the page of links 
-    .evaluate(function ('.d2l-loadmore-pager') {
-        return document.querySelector(selector).innerText;
-    }, selector)
+            clickLoad()
+        })
+}
+startNightmare(nightmare)
+//4. When it no longer exists, scrape the page of info
+var urlPrefix = 'www.brightspace.com';
 
-    //5. and download it to a CSV
-    .then(console.log)
-contents.savePage('fullPath', 'saveType', callback) {
-        if (error) {
-            console.log('There was an error')
-        } else if (!error) {
-            console.log('Saved page successfully, find your file at: ', fullPath)
-        }
-    }
-    //5. Do the things
-    .then(console.log)
+function scrapePage(nightmare) {
+    nightmare
+        .evaluate(function (urlPrefix) {
+            //take the urlPrefix,
+            //add every d2l-textblock element in the Links column after it
+        })
+}
+scrapePage(nightmare)
+//5. and download it to a CSV
+function saveList(nightmare) {
+    //code here
+}
+saveList(nightmare)
+//6. Do all the things
+nightmare
+    .then(console.log('patience...'))
     .catch(console.log);
