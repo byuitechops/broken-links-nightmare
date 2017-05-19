bee1ba73da1d461ac0fa4ce99cc0d3fbb2b10a70
maeashley <ashleystewart8888@yahoo.com>
2017-05-19 15:02:06 -0600

    added ending functions and included more modules

--- linkpagescrape.js
+++ linkpagescrape.js
@@ -2,8 +2,11 @@ var Nightmare = require('nightmare');
 var nightmare = new Nightmare({
     show: true
 });
+var dsv = require('d3-dsv')
+var fs = require('fs')
+
 var creds = require('./credentials.js')
-//1. Go to the page with all the links
+//1. Go to the page with all the links. Sign in, use the url to navigate to the correct page.
 function startNightmare(nightmare) {
     nightmare
         .goto('https://byui.brightspace.com/d2l/login?noredirect=true')
@@ -14,7 +17,7 @@ function startNightmare(nightmare) {
         .goto('https://byui.brightspace.com/d2l/brokenLinks/6606')
         //2. Wait for page to load
         .wait(1000)
-        //3. If the selector button exists, click it until it no longer exists
+        //3. If the selector button called "Load more" exists, click it
         .evaluate(function () {
             function clickLoad() {
                 if ($('.d2l-loadmore-pager:visible').length) {
@@ -27,22 +30,39 @@ function startNightmare(nightmare) {
 }
 startNightmare(nightmare)
 //4. When it no longer exists, scrape the page of info
-var urlPrefix = 'www.brightspace.com';
+var urlPrefix = 'https://brightspace.com/d2l/home/';
+var links = [];
 
 function scrapePage(nightmare) {
     nightmare
         .evaluate(function (urlPrefix) {
-            //take the urlPrefix,
-            //add every d2l-textblock element in the Links column after it
+            var link = urlPrefix + href;
+            links.push({
+                //take the urlPrefix,
+
+            })
+
+            //add every d2l-textblock element in the Links column after it individually
         })
 }
 scrapePage(nightmare)
-//5. and download it to a CSV
-function saveList(nightmare) {
-    //code here
-}
-saveList(nightmare)
-//6. Do all the things
 nightmare
     .then(console.log('patience...'))
     .catch(console.log);
+
+//6. Save everything to a file
+function complete(nightmare) {
+    nightmare
+        .end()
+        .then(function () {
+            console.log('Your file has been saved!')
+            var brokenLinks = (dsv.csvFormat(links))
+
+            fileName = null;
+            fs.writeFileSync(fileName, brokenLinks)
+            console.log('Your file has been saved as ' + fileName);
+        }).catch(function (Error) {
+            console.log('The error was: ', Error);
+        })
+}
+complete(nightmare)
