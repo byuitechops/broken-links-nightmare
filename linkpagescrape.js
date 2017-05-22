ef7129c57a6e51bb54c6464cfa992ffa531d39e0
maeashley <ashleystewart8888@yahoo.com>
2017-05-22 12:25:21 -0600

    Add files via upload

--- linkpagescrape.js
+++ linkpagescrape.js
@@ -6,7 +6,7 @@ var dsv = require('d3-dsv')
 var fs = require('fs')
 
 var creds = require('./credentials.js')
-//1. Go to the page with all the links. Sign in, use the url to navigate to the correct page.
+//1. Go to the page with all the links. Sign in with 'cct_maeashley' acct, use the url to navigate to link page.
 function startNightmare(nightmare) {
     nightmare
         .goto('https://byui.brightspace.com/d2l/login?noredirect=true')
@@ -28,44 +28,45 @@ function startNightmare(nightmare) {
             clickLoad()
         })
 }
-startNightmare(nightmare)
+startNightmare(nightmare);
 //4. When it no longer exists, scrape the page of info
-var urlPrefix = 'https://brightspace.com/d2l/home/';
-var href = ; //href attribute
-var links = [];
-
-var singleLink = urlPrefix + href;
-
+//take the table from the page and store it in an array
 function scrapePage(nightmare) {
     nightmare
-        .evaluate(function (urlPrefix) {
-            var link = urlPrefix + href;
-            links.push({
-                //take the urlPrefix,
-                singleLink('d2l-textblock')
-            })
+        .evaluate(function (selector) {
+            //create an array to store all the things
+            var getItAll = [];
+            //go through each individual thing and push it to the array
+            var entire = document.querySelectorAll('.vui-table tbody tr')
+            for (var i = 0; i < entire; i++) {
 
-            //add every d2l-textblock element in the Links column after it individually
-        })
-}
-scrapePage(nightmare)
-nightmare
-    .then(console.log('patience...'))
-    .catch(console.log);
+                return document.querySelector('d2l-textblock').innerHTML;
+                getItAll.push('d2l-textblock');
 
-//6. Save everything to a file
-function complete(nightmare) {
-    nightmare
-        .end()
+            }
+        }, 'd2l-textblock')
+        //6. Save everything to a CSV
         .then(function () {
-            console.log('Your file has been saved!')
-            var brokenLinks = (dsv.csvFormat(links))
 
-            fileName = null;
-            fs.writeFileSync(fileName, brokenLinks)
+            //run the program at the beginning
+            console.log('patience...');
+
+            //take a fileName and save the csv there
+            fileName = '';
+            /*I need to pass something into the dsv module. I'm not sure what.*/
+            var brokenLinks = (dsv.csvFormat());
+            fs.writeFileSync(fileName, brokenLinks);
             console.log('Your file has been saved as ' + fileName);
+
+
         }).catch(function (Error) {
             console.log('The error was: ', Error);
         })
 }
-complete(nightmare)
+scrapePage(nightmare);
+
+function complete(nightmare) {
+    nightmare
+        .end();
+}
+complete(nightmare);
