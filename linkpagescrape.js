78c4bd6146a3d90a31e73eaa99c54373f2ab6f52
maeashley <ashleystewart8888@yahoo.com>
2017-06-12 16:15:35 -0600

    updated selectors

--- linkpagescrape.js
+++ linkpagescrape.js
@@ -15,10 +15,18 @@ var credentials = [
         description: 'Please enter your password',
         hidden: true,
         replace: '*'
+    },
+    {
+        name: 'startDate',
+        description: 'Type a beginning date for the date range using the format "m/d/yyyy"'
+    },
+    {
+        name: 'endDate',
+        description: 'Type an end date for the date range using the format "m/d/yyyy"'
     }
 ];
 
-//1. Go to the page with all the links. Sign in with 'cct_maeashley' acct, use the url to navigate to link page.
+//1. Go to the page with all the links. Sign in use the url to navigate to link page.
 function startNightmare(nightmare) {
     nightmare
         .viewport(1200, 900)
@@ -33,7 +41,21 @@ function startNightmare(nightmare) {
         .wait(1000)
 
         //make the date ranges available (may need to prompt the user for input values)
+        /* .select('div.d2l-select-container select.vui-input.d2l-select option:nth-child(3)', '2')*/
+        .evaluate(function () {
 
+            document.querySelector('div.d2l-select-container select.vui-input.d2l-select option:nth-child(3)').selected = 'selected';
+            D2L.O("__g1", 47)();
+        })
+        .wait(1000)
+        .evaluate(function (dateInfo) {
+            document.getElementById('startDate').value = dateInfo.startDate;
+            document.getElementById('endDate').value = dateInfo.endDate;
+
+        }, dateInfo)
+        .type('#endDate', ' ')
+        .wait(2000)
+        .click('#optionsForm div:nth-of-type(1) > a')
         //3. If the selector button called "Load more" exists, click it
         .evaluate(function (callback) {
             function clickLoad() {
@@ -119,8 +141,13 @@ for ('.vui-table tbody tr' in getItAll) {
         })
 
 }
+
 //retrieve username and password from the user
+var promptInfo = {};
+var dateInfo = {};
+
 prompt.start();
+
 
 prompt.get(credentials, function (err, result) {
     if (err) {
@@ -130,6 +157,10 @@ prompt.get(credentials, function (err, result) {
     promptInfo = {
         username: result.username,
         password: result.password
+    }
+    dateInfo = {
+        startDate: result.startDate,
+        endDate: result.endDate
     }
     console.log('Thanks, checking credentials...')
     startNightmare(nightmare)
